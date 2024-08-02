4646ed4f3e9f3061150d08a14b6c31f2b5d23cac
Shreeraj Bhat <shreerajbhat@gmail.com>
2024-08-02 11:08:59 +0530

    Update streamlit_yolo.py

--- streamlit_yolo.py
+++ streamlit_yolo.py
@@ -1,5 +1,7 @@
+import streamlit as st
 from ultralytics import solutions
 from streamlit_webrtc import webrtc_streamer
+st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
 webrtc_streamer(key="example")
 # Pass a model as an argument
 solutions.inference(model="yolov8m.onnx")
