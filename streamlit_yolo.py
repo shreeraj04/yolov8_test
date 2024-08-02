cbfa1ff72da24679057b324433357d4fd7c2c699
Shreeraj Bhat <shreerajbhat@gmail.com>
2024-08-02 10:22:30 +0530

    Create streamlit_yolo.py

---
+++ streamlit_yolo.py
@@ -0,0 +1,6 @@
+from ultralytics import solutions
+
+# Pass a model as an argument
+solutions.inference(model="yolov8m.onnx")
+
+### Make sure to run the file using command `streamlit run <file-name.py>
